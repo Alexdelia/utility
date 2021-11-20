a941f84b1b8354b9cafd1da74d6a9ac501f06efd
Alexdelia <alexandre.delille.57@gmail.com>
2021-11-20 16:28:31 +0100

    update path of waifu2x-ncnn-vulkan

--- auto_waifu2x.sh
+++ auto_waifu2x.sh
@@ -5,7 +5,7 @@ printf "\033[1;36mStarting\033[0m\n\n"
 files=$(find . -type f -not -name '*_w2.png' -and -not -name '*_w2x.png' -and -not -name '*.ini')
 for f in $files ; do
 	echo "Doing:" "$f"
-	 /var/lib/flatpak/app/com.github.nihui.waifu2x-ncnn-vulkan/current/8bf9ef4885a0ca7426344fd1a080f8290c42373bb3e7aa5327a189c9319b4a27/export/bin/com.github.nihui.waifu2x-ncnn-vulkan -i "$f" -o "${f%.*}"_w2.png -n 2 -s 2
+	/home/alex/Ware/waifu2x-ncnn-vulkan-20210521-ubuntu/waifu2x-ncnn-vulkan -i "$f" -o "${f%.*}"_w2.png -n 2 -s 2
 	file "${f%.*}"_w2.png | awk '{print $1,"\033[1;31m",$5,$6,$7,"\033[0m\n"}' | tr ',' ' '
 	cp "$f" /home/alex/Pictures/.backup_wallpaper/
 	rm "$f"
