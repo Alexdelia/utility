3b12aad53dd6fd599ec711019e3dad95b4afb122
Alexdelia <alexandre.delille.57@gmail.com>
2022-07-08 19:34:26 +0200

    `needed_software_install.sh`

---
+++ needed_software_install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [[ $EUID != 0 ]]
+then
+	printf "\033[31;1mError:\t\033[0m\033[31mneed sudo\033[0m\n"
+	exit 1
+fi
+
+sudo apt update
+sudo apt install kitty steam plank bashtop discord shutter
