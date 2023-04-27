3b4fec465b684b00d7f3deb5daa0b72c4d9b8c31
Alexdelia <alexandre.delille.57@gmail.com>
2023-04-27 11:44:36 +0200

    rm

--- comp.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-clang -Wall -Werror -Wextra -g -fsanitize=address ./*.c
--- rmbk.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# rm -r .*~
-# rm -r *~
-find . -name '.*~' -type f -delete
-find . -name '*~' -type f -delete
-echo -e "\e[32;1mdone\e[0m"
